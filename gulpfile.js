59ca3feab2244c171d66c666b299015af10efeb2
Scott Mitchell <scmitchell@lgscout.com>
2015-04-05 21:10:14 -0400

    Add build to ignore, update gulpfile to compile app and vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,24 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
 
 var DEST = 'build/';
 
 gulp.task('js', function() {
 
   gulp.src('./assets/js/app.js')
-  .pipe(gulp.dest(DEST))
   .pipe(uglify())
   .pipe(rename({ extname: '.min.js' }))
   .pipe(gulp.dest(DEST));
 
   gulp.src('./assets/js/vendor/**/*.js')
-      .pipe(uglify())
-      .pipe(concat("vendor.js"))
-      .pipe(gulp.dest('./assets/js'))
-      .pipe( notify({ message: "Javascript is now ugly!"}) );
+  .pipe(uglify())
+  .pipe(concat("vendor.js"))
+  .pipe(gulp.dest('./assets/js'))
+  .pipe( notify({ message: "Javascript pipe finished."}) );
 
 });
 
